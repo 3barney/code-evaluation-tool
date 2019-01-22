af688d23a30fded784288b487e9569d2c725efdc
barney <brnbsjm@gmail.com>
2019-01-22 22:19:46 +0300

    docker file and compose setup, test app running instance locally

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,13 @@
+import os
+import sys
 from flask import Flask, jsonify
 
-app = Flask(__name__) # Instantiate app
+# Instantiate app
+app = Flask(__name__)
+
+# Read env vars from compose dev file
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
